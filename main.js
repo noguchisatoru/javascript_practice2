1b279035dd56546421106a824e256cff3b218a8a
野口慧 <satoru@noguchitoshinoMacBook-Pro.local>
2019-05-10 20:30:20 +0900

    ver1.3

--- main.js
+++ main.js
@@ -1,48 +1,77 @@
-let btn_Add = document.getElementById('add-button');
-let btn_Work = document.getElementById('work');
+let btnAdd = document.getElementById('add-button');
+
 let i = 0;
 
-btn_Add.addEventListener('click', function(){
+btnAdd.addEventListener('click', function(){
 
 let content = document.getElementById('content');
-let todo_list = document.getElementById('todo-list');
+let todoList = document.getElementById('todo-list');
 
 const tr = document.createElement('tr');
-const td_id = document.createElement('td');
-const td_comment = document.createElement('td');
-const td_work = document.createElement('td');
-const td_remove = document.createElement('td');
-
+tr.id = 'list';
 
 //ID
-td_id.textContent = i;
-let id = tr.appendChild(td_id);
+const tdId = document.createElement('td');
+tdId.textContent = i;
+let id = tr.appendChild(tdId);
 i++;
 
 //コメント
-td_comment.textContent = content.value;
-let comment = tr.appendChild(td_comment);
+const tdComment = document.createElement('td');
+tdComment.textContent = content.value;
+let comment = tr.appendChild(tdComment);
 
 //作業中ボタン
-td_work.innerHTML = '<button id="work">作業中</button>'
-let work = tr.appendChild(td_work);
+const tdWork = document.createElement('td');
+const btnWork = document.createElement('button');
+const textWork = document.createTextNode('作業中');
+
+tdWork.id = 'state';
+btnWork.id = 'work';
+btnWork.appendChild(textWork);
+tdWork.appendChild(btnWork);
+
+let work = tr.appendChild(tdWork);
 
 //removeボタン
-td_remove.innerHTML = '<button id="remove">削除</button>'
-let remove = tr.appendChild(td_remove);
+const tdRemove = document.createElement('td');
+const btnRemove = document.createElement('button');
+const textRemove = document.createTextNode('削除');
+
+btnRemove.id = 'remove';
+btnRemove.appendChild(textRemove);
+tdRemove.appendChild(btnRemove);
+
+let remove = tr.appendChild(tdRemove);
 
 //表示
-todo_list.appendChild(tr);
+todoList.appendChild(tr);
 tr.appendChild(id);
 tr.appendChild(comment);
 tr.appendChild(work);
-tr.appendChild(remove)
-
+tr.appendChild(remove);
+ 
 content.value = "";
+
+let btnWorking = document.getElementById('work');
+    //完了に切り替え
+    btnWorking.addEventListener('click', function(){
+        const target = document.getElementById('state');
+        const currentBtn = document.getElementById('work');
+        const newBtn = document.createElement('button');
+        newBtn.id = 'end';
+        newBtn.textContent = '完了';
+        target.replaceChild(newBtn, currentBtn);
+    },false);
+
+    let btnRemoving = document.getElementById('remove');
+    //削除
+    btnRemoving.addEventListener('click', function(){
+        const listElement = document.querySelector('#list');
+        listElement.remove();
+    },false);
+
+
 },false);
 
-//完了に切り替え
-btn_Add.addEventListener('click', function(){
-    const work_Mode = document.querySelector('#work');
-    console.log(work_Mode.outerHTML);
-},false);
+
