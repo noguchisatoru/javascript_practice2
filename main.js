ab1f66b4f32d8bd8f3e7dd6d8e75658d794dd395
野口慧 <satoru@noguchitoshinoMacBook-Pro.local>
2019-05-15 13:34:00 +0900

    完成形（IDの振り直しはまだ）

--- main.js
+++ main.js
@@ -1,113 +1,129 @@
-let btnAdd = document.getElementById('add-button');
+const btnAdd = document.getElementById('add-button');
+const content = document.getElementById('content');
+const todoList = document.getElementById('todo-list');
+const element = document.getElementById("radioBtn");
 
-let i = 0;
+//ID用の変数の初期化
+let index = 0;
 
-btnAdd.addEventListener('click', function(){
+btnAdd.addEventListener('click', function () {
 
-let content = document.getElementById('content');
-let todoList = document.getElementById('todo-list');
+    const tr = document.createElement('tr');
+    tr.classList.add('list');
 
-const tr = document.createElement('tr');
-tr.classList.add('list');
+    //IDの生成
+    const tdId = document.createElement('td');
+    tdId.textContent = index;
 
-//ID
-const tdId = document.createElement('td');
-tdId.textContent = i;
-let id = tr.appendChild(tdId);
-i++;
+    //コメントの生成
+    const tdComment = document.createElement('td');
+    tdComment.textContent = content.value;
 
-//コメント
-const tdComment = document.createElement('td');
-tdComment.textContent = content.value;
-let comment = tr.appendChild(tdComment);
-
-//作業中ボタン
-const tdWork = document.createElement('td');
-const btnWork = document.createElement('button');
-const textWork = document.createTextNode('作業中');
-
-tdWork.classList.add('state');
-btnWork.classList.add('work');
-btnWork.appendChild(textWork);
-tdWork.appendChild(btnWork);
-
-let work = tr.appendChild(tdWork);
-
-//removeボタン
-const tdRemove = document.createElement('td');
-const btnRemove = document.createElement('button');
-const textRemove = document.createTextNode('削除');
-
-btnRemove.id = 'remove';
-btnRemove.appendChild(textRemove);
-tdRemove.appendChild(btnRemove);
-
-let remove = tr.appendChild(tdRemove);
-
-//表示
-todoList.appendChild(tr);
-tr.appendChild(id);
-tr.appendChild(comment);
-tr.appendChild(work);
-tr.appendChild(remove);
- 
-content.value = "";
-
-    
-    btnWork.addEventListener('click', function(){
+    //作業中ボタンの生成
+    const tdWork = document.createElement('td');
+    tdWork.classList.add('state');
+    const btnWork = document.createElement('button');
+    btnWork.textContent = '作業中';
+    btnWork.classList.add('work');
+    btnWork.addEventListener('click', function () {
         //完了に切り替え
-        if(btnWork.classList.value === "work"){
+        if (btnWork.classList.value === "work") {
             btnWork.classList.value = "end";
             btnWork.textContent = "完了";
-        }else if(btnWork.classList.value === "end"){
+        } else if (btnWork.classList.value === "end") {
             //作業中に切り替え
             btnWork.classList.value = "work";
             btnWork.textContent = "作業中";
         }
-    },false);
+    });
 
-    //削除
-    btnRemove.addEventListener('click', function(){
-        const listElement = document.querySelector('.list');
-        listElement.remove();
-    },false);
+    //removeボタン
+    const tdRemove = document.createElement('td');
+    const btnRemove = document.createElement('button');
+    btnRemove.id = 'remove';
+    btnRemove.textContent = '削除';
+    btnRemove.addEventListener('click', function () {
+        //削除処理
+        this.parentNode.parentNode.remove();
+    });
 
+    //表示
+    tdWork.appendChild(btnWork);
+    tdRemove.appendChild(btnRemove);
+    tr.appendChild(tdId);
+    tr.appendChild(tdComment);
+    tr.appendChild(tdWork);
+    tr.appendChild(tdRemove);
+    todoList.appendChild(tr);
 
-},false);
+    content.value = "";
+    index++;
 
-let element = document.getElementById("radioBtn");
+});
 
-element.addEventListener('change',function(){
-    
-    let radioNodeList = element["switchDisplay"];
-    let select = radioNodeList.value;
-    let allList = document.getElementsByClassName("state");
-    console.log(allList);
-    if(select === "all"){
-        for(let j = 0; j < allList.length; j++){
-            if(allList[j].childNodes[0].className === "work"){
-                allList[j].parentNode.style.visibility="visible";
-            }else{
-                allList[j].parentNode.style.visibility="visible";
+element.addEventListener('change', function () {
+
+    const radioNodeList = element["switchDisplay"];
+    const select = radioNodeList.value;
+    const allList = document.getElementsByClassName("state");
+
+    if (select === "all") {
+        for (let j = 0; j < allList.length; j++) {
+            if (allList[j].childNodes[0].className === "work") {
+                allList[j].parentNode.style.display = "";
+            } else {
+                allList[j].parentNode.style.display = "";
             }
         }
-    }else if(select === "working"){
-        for(let j = 0; j < allList.length; j++){
-            if(allList[j].childNodes[0].className === "work"){
-                allList[j].parentNode.style.visibility="visible";
-            }else{
-                allList[j].parentNode.style.visibility="hidden";
+    } else if (select === "working") {
+        for (let j = 0; j < allList.length; j++) {
+            if (allList[j].childNodes[0].className === "work") {
+                allList[j].parentNode.style.display = "";
+            } else {
+                allList[j].parentNode.style.display = "none";
             }
         }
-    }else if(select === "complete"){
-        for(let j = 0; j < allList.length; j++){
-            if(allList[j].childNodes[0].className === "work"){
-                allList[j].parentNode.style.visibility="hidden";
-            }else{
-                allList[j].parentNode.style.visibility="visible";
+    } else if (select === "complete") {
+        for (let j = 0; j < allList.length; j++) {
+            if (allList[j].childNodes[0].className === "work") {
+                allList[j].parentNode.style.display = "none";
+            } else {
+                allList[j].parentNode.style.display = "";
             }
         }
     }
-},false);
+
+    /*forEachを使った場合
+    if (select === "all") {
+        const workBtnList = document.getElementsByClassName("work");
+        //workBtnList.from()
+        workBtnList.forEach(workBtn => {
+            workBtn.style.visibility = "visible";
+        });
+        const endBtnList = document.getElementsByClassName("end");
+        endBtnList.array.forEach(endBtn => {
+            endBtn.style.visibility = "visible";
+        });
+    } else if (select === "working") {
+        const workBtnList = document.getElementsByClassName("work");
+        workBtnList.array.forEach(workBtn => {
+            workBtn.style.visibility = "visible";
+        });
+        const endBtnList = document.getElementsByClassName("end");
+        endBtnList.array.forEach(endBtn => {
+            endBtn.style.visibility = "hidden";
+        });
+    } else if (select === "complete") {
+        const workBtnList = document.getElementsByClassName("work");
+        workBtnList.array.forEach(workBtn => {
+            workBtn.style.visibility = "hidden";
+        });
+        const endBtnList = document.getElementsByClassName("end");
+        endBtnList.array.forEach(endBtn => {
+            endBtn.style.visibility = "visible";
+        });
+    }
+            */
+});
 
 
